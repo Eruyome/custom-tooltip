deede7cbb08e7e36863701bcb0c53658042374f8
Eruyome <prinzeru@gmail.com>
2019-06-25 16:33:21 +0200

    removed routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
+//import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
@@ -11,10 +11,10 @@ import { TopBarComponent } from './top-bar/top-bar.component';
 @NgModule({
   imports: [
     BrowserModule,
-    ReactiveFormsModule,
-    RouterModule.forRoot([
-      { path: '', component: PriceTableComponent },
-    ])
+    ReactiveFormsModule //,
+    //RouterModule.forRoot([
+    //  { path: '', component: PriceTableComponent },
+    //])
   ],
   declarations: [
     AppComponent,
